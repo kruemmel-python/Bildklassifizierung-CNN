a6395931b132afc76ee18fe48c2cc3bee47d8550
Ralf Krümmel <ralf.kruemmel+python@outlook.de>
2024-05-26 08:00:11 +0200

    Update vorhersage.py

--- vorhersage.py
+++ vorhersage.py
@@ -1,31 +1,32 @@
+import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import numpy as np
-
 
 # Modell laden
 model = load_model('images.keras')
- # Bildgröße definieren
-image_size = (512, 512)  # Beispielgröße, anpassen nach Bedarf
-# Neues Bild laden und vorbereiten
-img_path = 'D:\\images\\fzgs\\43.jpg'  # Pfad zum neuen Bild
-img = load_img(img_path, target_size=image_size)  # Bildgröße muss mit dem trainierten Modell übereinstimmen
-img_array = img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)  # Bild zu einem Batch hinzufügen
-img_array /= 255.  # Normalisierung, wie im ImageDataGenerator
 
-# Vorhersage machen
-predictions = model.predict(img_array)
-predicted_class = np.argmax(predictions, axis=1)  # Klasse mit der höchsten Wahrscheinlichkeit
+# Liste der Bildpfade
+image_paths = ['D:\\images\\test3.jpg', 'D:\\images\\test2.jpg', 'D:\\images\\test1.jpg', 'D:\\images\\test.jpg']  # usw.
 
+# Bildgröße definieren (muss mit dem trainierten Modell übereinstimmen)
+image_size = (512, 512)
 
 # Klassenbezeichnungen für bessere Lesbarkeit
 klassen_namen = ['nicht greenscreen fähig', 'greenscreen fähig']
 
-# Vorhergesagte Klasse ausgeben
-if predicted_class[0] == 0:
-    print(f"Das Modell sagt Klasse {predicted_class[0]} für das Bild voraus. Es ist {klassen_namen[0]}!")
-else:
-    print(f"Das Modell sagt Klasse {predicted_class[0]} für das Bild voraus. Es ist {klassen_namen[1]}!")
+# Vorhersagen für jedes Bild machen
+for img_path in image_paths:
+    img = load_img(img_path, target_size=image_size)
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)  # Bild zu einem Batch hinzufügen
+    img_array /= 255.  # Normalisierung, wie im ImageDataGenerator
 
+    predictions = model.predict(img_array)
+    predicted_class = np.argmax(predictions, axis=1)  # Klasse mit der höchsten Wahrscheinlichkeit
+
+    # Vorhergesagte Klasse ausgeben
+    if predicted_class[0] == 0:
+        print(f"Das Modell sagt Klasse {predicted_class[0]} für das Bild {img_path} voraus. Es ist {klassen_namen[0]}!")
+    else:
+        print(f"Das Modell sagt Klasse {predicted_class[0]} für das Bild {img_path} voraus. Es ist {klassen_namen[1]}!")
 
